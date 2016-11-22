e6cb27c5d82ff9f100d5baa90163c10fc7067d8c
Chris Blume <ProgramMax@gmail.com>
2016-11-21 23:17:33 -0800

    Adding host to endian swap

--- Code/Include/max/Compiling/Configuration/Platform/Win32.hpp
+++ Code/Include/max/Compiling/Configuration/Platform/Win32.hpp
@@ -13,12 +13,15 @@
 	#if defined( __x86_64__ )
 		#define MAX_64BIT_WORD_SIZE
 		#define MAX_X86_64
+		#define MAX_LITTLE_ENDIAN
 	#elif defined( __i386__ )
 		#define MAX_32BIT_WORD_SIZE
 		#define MAX_X86
+		#define MAX_LITTLE_ENDIAN
 	#elif defined( __IA64__ )
 		#define MAX_64BIT_WORD_SIZE
 		#define MAX_IA64
+		#define MAX_LITTLE_ENDIAN
 	#else
 		static_assert( false, "Unknown platform" );
 	#endif
@@ -26,12 +29,15 @@
 	#if defined( _M_X64 )
 		#define MAX_64BIT_WORD_SIZE
 		#define MAX_X86_64
+		#define MAX_LITTLE_ENDIAN
 	#elif defined( _M_IX86 )
 		#define MAX_32BIT_WORD_SIZE
 		#define MAX_X86
+		#define MAX_LITTLE_ENDIAN
 	#elif defined( _M_IA64 )
 		#define MAX_64BIT_WORD_SIZE
 		#define MAX_IA64
+		#define MAX_LITTLE_ENDIAN
 	#else
 		static_assert( false, "Unknown platform" );
 	#endif
