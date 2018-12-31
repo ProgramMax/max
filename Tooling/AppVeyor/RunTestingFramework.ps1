3739a2a04e517fdb6e458684e2440f5242da9576
Chris Blume <ProgramMax@gmail.com>
2018-12-31 05:06:00 -0800

    More hard-coding

--- Tooling/AppVeyor/RunTestingFramework.ps1
+++ Tooling/AppVeyor/RunTestingFramework.ps1
@@ -1,4 +1,5 @@
-$($env:APPVEYOR_BUILD_FOLDER)/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
+#$($env:APPVEYOR_BUILD_FOLDER)/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
+C:/projects/max/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
 
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/juint/$($env:APPVEYOR_JOB_ID)", (Resolve-Path C:/projects/max/Tooling/AppVeyor/junit-results.xml))
