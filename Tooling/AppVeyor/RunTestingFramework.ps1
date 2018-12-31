869b1bf4110ae2d8bc4dce3f742fc32973372f22
Chris Blume <ProgramMax@gmail.com>
2018-12-31 04:48:51 -0800

    Fix typo

--- Tooling/AppVeyor/RunTestingFramework.ps1
+++ Tooling/AppVeyor/RunTestingFramework.ps1
@@ -1,4 +1,4 @@
 C:/projects/max/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
 
-i$wc = New-Object 'System.Net.WebClient'
+$wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/juint/$($env:APPVEYOR_JOB_ID)", (Resolve-Path ./junit-results.xml))
