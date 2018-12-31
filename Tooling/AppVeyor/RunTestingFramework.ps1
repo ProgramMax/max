69cb243de26f21c7df75123c1d3a2362ffac2816
Chris Blume <ProgramMax@gmail.com>
2018-12-31 04:13:30 -0800

    Hard-code the path, use  inside the script

--- Tooling/AppVeyor/RunTestingFramework.ps1
+++ Tooling/AppVeyor/RunTestingFramework.ps1
@@ -1,4 +1,4 @@
-%APPVEYOR_BUILD_FOLDER%/Projects/VisualStudio/maxAutomatedTests/%PLATFORM%/%CONFIGURATION%/maxAutomatedTests.exe
+$(APPVEYOR_BUILD_FOLDER)/Projects/VisualStudio/maxAutomatedTests/$(PLATFORM)/$(CONFIGURATION)/maxAutomatedTests.exe
 
 i$wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/juint/$($end:APPVEYOR_JOB_ID)", (Resolve-Path ./junit-results.xml))
