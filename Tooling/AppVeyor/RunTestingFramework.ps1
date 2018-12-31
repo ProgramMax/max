5aed4575996e24c100fe625ad0fa5a51ef33be7b
Chris Blume <ProgramMax@gmail.com>
2018-12-31 11:12:15 -0800

    Don't use an external script

--- Tooling/AppVeyor/RunTestingFramework.ps1
+++
@@ -1,5 +0,0 @@
-#$($env:APPVEYOR_BUILD_FOLDER)/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
-C:/projects/max/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
-
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/juint/$($env:APPVEYOR_JOB_ID)", (Resolve-Path C:/projects/max/Tooling/AppVeyor/junit-results.xml))
