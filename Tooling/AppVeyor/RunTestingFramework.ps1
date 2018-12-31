bb3a6c95c2261d9678aabe4aa9c43c72f10b30a9
Chris Blume <ProgramMax@gmail.com>
2018-12-31 04:45:33 -0800

    Try hard-coding again (plus in the script)

--- Tooling/AppVeyor/RunTestingFramework.ps1
+++ Tooling/AppVeyor/RunTestingFramework.ps1
@@ -1,4 +1,4 @@
-$($env:APPVEYOR_BUILD_FOLDER)\Projects\VisualStudio\maxAutomatedTests\$env:PLATFORM\$env:CONFIGURATION\maxAutomatedTests.exe
+C:/projects/max/Projects/VisualStudio/maxAutomatedTests/Win32\Debug\maxAutomatedTests.exe
 
 i$wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/juint/$($env:APPVEYOR_JOB_ID)", (Resolve-Path ./junit-results.xml))
