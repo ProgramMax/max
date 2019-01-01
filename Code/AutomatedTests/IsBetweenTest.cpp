fda85b6e6c54f8c6a1244fd495825d0695d68965
Chris Blume <ProgramMax@gmail.com>
2018-12-31 22:23:32 -0800

    Remove usage example from TestSuite.hpp
    TestSuite.hpp had an old usage example. This should be moved into
    documentation. However, there are existing examples in use and
    this commented example no longer provided value.
    
    Documentation can happen in a separate step.
    
    This commit removes the commented usage example.

--- Code/AutomatedTests/IsBetweenTest.cpp
+++ Code/AutomatedTests/IsBetweenTest.cpp
@@ -5,6 +5,8 @@
 #include "IsBetweenTest.hpp"
 #include <max/Algorithms/IsBetween.hpp>
 #include <max/Testing/TestSuite.hpp>
+#include <max/Testing/CoutResultPolicy.hpp>
+#include <utility>
 
 namespace maxAutomatedTests
 {
@@ -13,24 +15,25 @@ namespace Algorithms
 
 	void RunIsBetweenTestSuite()
 	{
-		auto IsBetweenTestSuite = max::Testing::TestSuite{ "max::Algorithms::IsBetween test suite" };
+		max::Testing::CoutResultPolicy ResultPolicy;
+		auto IsBetweenTestSuite = max::Testing::TestSuite< max::Testing::CoutResultPolicy >{ "max::Algorithms::IsBetween test suite", std::move( ResultPolicy ) };
 		
-		IsBetweenTestSuite.AddTest( max::Testing::Test{ "inside range", []( max::Testing::Test & CurrentTest ) {
+		IsBetweenTestSuite.AddTest( max::Testing::Test< max::Testing::CoutResultPolicy >{ "inside range", []( max::Testing::Test< max::Testing::CoutResultPolicy > & CurrentTest, max::Testing::CoutResultPolicy const & ResultPolicy ) {
 			CurrentTest.MAX_TESTING_ASSERT( max::Algorithms::IsBetween( 10, { 1, 100 } ) );
 		}
 		} );
 
-		IsBetweenTestSuite.AddTest( max::Testing::Test{ "before range", []( max::Testing::Test & CurrentTest ) {
+		IsBetweenTestSuite.AddTest( max::Testing::Test< max::Testing::CoutResultPolicy >{ "before range", []( max::Testing::Test< max::Testing::CoutResultPolicy > & CurrentTest, max::Testing::CoutResultPolicy const & ResultPolicy ) {
 			CurrentTest.MAX_TESTING_ASSERT( ! max::Algorithms::IsBetween( 0, { 1, 100 } ) );
 		}
 		} );
 
-		IsBetweenTestSuite.AddTest( max::Testing::Test{ "after range", []( max::Testing::Test & CurrentTest ) {
+		IsBetweenTestSuite.AddTest( max::Testing::Test< max::Testing::CoutResultPolicy >{ "after range", []( max::Testing::Test< max::Testing::CoutResultPolicy > & CurrentTest, max::Testing::CoutResultPolicy const & ResultPolicy ) {
 			CurrentTest.MAX_TESTING_ASSERT( ! max::Algorithms::IsBetween( 200, { 1, 100 } ) );
 		}
 		} );
 
-		IsBetweenTestSuite.AddTest( max::Testing::Test{ "minimum and maximum switched", []( max::Testing::Test & CurrentTest ) {
+		IsBetweenTestSuite.AddTest( max::Testing::Test< max::Testing::CoutResultPolicy >{ "minimum and maximum switched", []( max::Testing::Test< max::Testing::CoutResultPolicy > & CurrentTest, max::Testing::CoutResultPolicy const & ResultPolicy ) {
 			CurrentTest.MAX_TESTING_ASSERT( ! max::Algorithms::IsBetween( 10, { 100, 1 } ) );
 		}
 		} );
