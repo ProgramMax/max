f94ab31bfa3fa566b29490a3ec6733965399b01b
Chris Blume <ProgramMax@gmail.com>
2019-07-10 03:10:14 -0700

    Fix ARM compiling
    This commit fixes the ARM compiles.
    
    There is still work to be done to get parity to x86:
    - The Vector unit tests fail because of the call to sqrt().
    - The CPUID code needs to be reworked.
    
    But this is progress in the right direction. :)
    
    Partially fixes #26

--- Code/max/Compiling/Configuration/Platform/Linux.hpp
+++ Code/max/Compiling/Configuration/Platform/Linux.hpp
@@ -13,17 +13,29 @@
 	#if defined( __x86_64__ )
 		#define MAX_64BIT_WORD_SIZE
 		#define MAX_X86_64
-		#define MAX_LITTLE_ENDIAN
 	#elif defined( __i386__ )
 		#define MAX_32BIT_WORD_SIZE
 		#define MAX_X86
-		#define MAX_LITTLE_ENDIAN
 	#elif defined( __IA64__ )
 		#define MAX_64BIT_WORD_SIZE
 		#define MAX_IA64
-		#define MAX_LITTLE_ENDIAN
+	#elif defined( __aarch64__ )
+		#define MAX_64BIT_WORD_SIZE
+		#define MAX_AARCH64
+	#elif defined( __arm__ )
+		#define MAX_32BIT_WORD_SIZE
+		#define MAX_ARM
+	#elif defined( __thumb__ )
+		#define MAX_16BIT_WORD_SIZE
+		#define MAX_THUMB
 	#else
 		static_assert( false, "Unknown platform" );
+	#endif
+
+	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		#define MAX_LITTLE_ENDIAN
+	#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+		#define MAX_BIG_ENDIAN
 	#endif
 #else
 	static_assert( false, "Unknown platform" );
