49513c3ad69aa79d943bc809f70623f7b0ab18a6
Chris Blume <ProgramMax@gmail.com>
2026-09-02 20:40:23 -0400

    Update VC version

--- Code/max/Compiling/Configuration/Compiler/VC.hpp
+++ Code/max/Compiling/Configuration/Compiler/VC.hpp
@@ -11,8 +11,13 @@
 
 #define MAX_COMPILER_MESSAGE( Message ) __pragma( message( Message ) )
 
-#if _MSC_VER > 1950
+#if _MSC_VER > 1951
 	MAX_COMPILER_MESSAGE("Compiling with a newer version of MSVC than max recognizes. Using last known version.");
+#elif _MSC_VER >= 1951
+	// MSVC++ (Visual Studio 2026 / version 18.9)
+	// TODO: This version number probably also matches some earlier version between the previous 18.2 and the current 18.9
+	#define MAX_COMPILER_VERSION_MAJOR 18
+	#define MAX_COMPILER_VERSION_MINOR 9
 #elif _MSC_VER >= 1950
 	// MSVC++ (Visual Studio 2026 / version 18.2)
 	#define MAX_COMPILER_VERSION_MAJOR 18
